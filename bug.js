ab9ba8c44595495c584903c66564da15b637f2df
BugHunter-X <askzenai@gmail.com>
2024-12-29 16:28:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,34 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href='/about'>
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+import { useRouter } from 'next/router';
+
+export default function About() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/'); // Navigate back to the home page
+  };
+
+  return (
+    <div>
+      <h1>About Page</h1>
+      <button onClick={handleClick}>Go back to Home</button>
+    </div>
+  );
+}
+```
